dc09c1e96aa94eaa92fb24a287ef724a8db38afd
Denis Brumann <denis.brumann@sensiolabs.de>
2019-06-05 22:37:25 +0200

    Remove token in handler.

--- src/MessageHandler/RegisterUserHandler.php
+++ src/MessageHandler/RegisterUserHandler.php
@@ -3,21 +3,29 @@
 namespace App\MessageHandler;
 
 use App\Message\RegisterUserMessage;
+use const DIRECTORY_SEPARATOR;
 use Doctrine\ORM\EntityManagerInterface;
+use function rtrim;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 final class RegisterUserHandler implements MessageHandlerInterface
 {
     private $entityManager;
+    private $confirmationStorageDir;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, string $confirmationStorageDir)
     {
         $this->entityManager = $entityManager;
+        $this->confirmationStorageDir = rtrim($confirmationStorageDir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
     }
 
     public function __invoke(RegisterUserMessage $message): void
     {
         $this->entityManager->persist($message->getUser());
         $this->entityManager->flush();
+
+        $filesystem = new Filesystem();
+        $filesystem->remove($this->confirmationStorageDir . $message->getToken() . '.json');
     }
 }
